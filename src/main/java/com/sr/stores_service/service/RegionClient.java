9102bb4003a6a6f6f90f46ee0b8b97ef94ead4b7
RAKSHITH R <108052912+RakshithR07@users.noreply.github.com>
2025-01-24 01:52:43 +0530

    Updated application.properties

--- src/main/java/com/sr/stores_service/service/RegionClient.java
+++ src/main/java/com/sr/stores_service/service/RegionClient.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.sr.stores_service.pojo.RegionPojo;
 
-@FeignClient(name = "region-service", url = "http://region-service:8081/api")  // URL of the Department Service
+//@FeignClient(name = "region-service", url = "http://region-service:8081/api")  // URL of the Department Service
 public interface RegionClient {
 
     // Endpoint to fetch department details by department ID
-    @GetMapping("/regions/{id}")
-    RegionPojo getDepartmentById(@PathVariable("id") int regionId);
+//    @GetMapping("/regions/{id}")
+//    RegionPojo getDepartmentById(@PathVariable("id") int regionId);
 }
 

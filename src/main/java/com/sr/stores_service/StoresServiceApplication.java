ee26f604912169be8e99ee1b05591555df660237
SriramSriChandana <srichandanasriram2003@gmail.com>
2025-01-23 17:45:17 +0530

    Initial commit

---
+++ src/main/java/com/sr/stores_service/StoresServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sr.stores_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoresServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoresServiceApplication.class, args);
+	}
+
+}
